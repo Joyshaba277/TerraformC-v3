10eb120356ae17476190793ee98b12a9b3e3af77
Joyce Shaba <joyceshaba277@gmail.com>
2026-04-07 15:43:34 -0500

    add-vm-update-tfc-org-ws

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ terraform {
 
   # Terraform Cloud backend — workspace settings managed in TFC UI
   cloud {
-    organization = "your-tfc-organization"
+    organization = "Haircloset"
     workspaces {
-      name = "your-workspace-name"
+      name = "TerraformCI-v2"
     }
   }
 }
